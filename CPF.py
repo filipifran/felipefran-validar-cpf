aad416550496531ec90c3281daee8e6db3c3b551
filipi <felipecarter77@gmail.com>
2026-05-04 09:04:26 -0300

    Add files via upload

---
+++ CPF.py
@@ -0,0 +1,30 @@
+def validar_cpf(cpf):
+    cpf = ''.join(filter(str.isdigit, cpf))
+
+    if len(cpf) != 11 or cpf == cpf[0] * 11:
+        return False
+
+    # Primeiro dígito verificador
+    soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
+    digito1 = (soma * 10 % 11) % 10
+
+    # Segundo dígito verificador
+    soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
+    digito2 = (soma * 10 % 11) % 10
+
+    return cpf[-2:] == f"{digito1}{digito2}"
+
+
+def main():
+    cpf = input("Digite um CPF: ")
+
+    if validar_cpf(cpf):
+        print("CPF válido!")
+    else:
+        print("CPF inválido!")
+
+
+if __name__ == "__main__":
+    main()
+
+# Exemplo caso queira usar: 12345678909
