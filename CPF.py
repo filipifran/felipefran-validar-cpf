e387994e8e43736ec02cf3d80aabe4592f3db0a6
filipi <felipecarter77@gmail.com>
2026-05-04 10:25:32 -0300

    Update CPF.py

--- CPF.py
+++ CPF.py
@@ -4,11 +4,11 @@ def validar_cpf(cpf):
     if len(cpf) != 11 or cpf == cpf[0] * 11:
         return False
 
-    # Primeiro dígito verificador
+    
     soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
     digito1 = (soma * 10 % 11) % 10
 
-    # Segundo dígito verificador
+    
     soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
     digito2 = (soma * 10 % 11) % 10
 
